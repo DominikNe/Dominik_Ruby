86b3d2bf6a705712489a51ae63a1bcc39ad1bbc9
DominikNe <dominik.neumann@myebs.de>
2017-05-29 14:11:20 +0200

    Add files via upload

---
+++ Ruby 1/program2.rb
@@ -0,0 +1,5 @@
+def greeting # here we say def to define a method and put the name of our method
+  p "hello" # here’s the code inside our method
+end # and here we end or close our method
+
+greeting
---
+++ Ruby 1/program3.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  p "hello" + " " + name
+end
+
+greeting("Sasha")
